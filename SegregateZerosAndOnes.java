b398ad1f33fbeba2b7f949323c51da0ae869899a
trinadhamireddy <trinadh.406@gmail.com>
2017-05-02 23:05:05 +0530

    Add files via upload
    Segregate 0s on left side and 1s on right side of the array. Traverse array only once

---
+++ SegregateZerosAndOnes.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+public class SegregateZerosAndOnes {
+	public static void segregateZerosAndOnes(int[] a) {
+		int i=0,n=a.length-1,j=n,temp;
+		while(i<j) {
+			while(i<n && a[i]==0)
+				i++;
+			while(j>=0 && a[j]==1) 
+				j--;
+			temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+			i++;
+			j--;
+		}
+		for(i=0;i<n;i++)
+			System.out.print(a[i]+" ");
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int[] a = new int[n];
+		for(int i=0;i<n;i++)
+			a[i] = sc.nextInt();
+		
+		segregateZerosAndOnes(a);		
+		
+		//closing scanner Object
+		sc.close();
+	}
+}
