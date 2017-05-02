9371df01104cba9b78467549ccfd35f51543e49f
trinadhamireddy <trinadh.406@gmail.com>
2017-05-02 22:47:42 +0530

    Add files via upload

---
+++ UnionAndIntersectionOfTwoArrays.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class UnionAndIntersectionOfTwoArrays {
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		int n1 = s.nextInt();
+		int[] a1 = new int[n1];
+		for(int i=0;i<n1;i++)
+			a1[i] = s.nextInt();
+		int n2 = s.nextInt();	
+		int[] a2 = new int[n2];
+		for(int i=0;i<n2;i++)
+			a2[i] = s.nextInt();
+		
+		int i1=0,i2=0;
+		ArrayList<Integer> u = new ArrayList<Integer>();
+		while(i1<n1 && i2<n2) {
+			if(a1[i1]==a2[i2]) {
+				u.add(a1[i1++]);
+				i2++;
+			} else if(a1[i1]<a2[i2]) {
+				u.add(a1[i1++]);
+			} else {
+				u.add(a2[i2++]);
+			}
+		}
+		while(i1<n1)
+			u.add(a1[i1++]);
+		while(i2<n2)
+			u.add(a2[i2++]);
+		
+		for(Integer i:u)
+			System.out.print(i+" ");
+		System.out.println();
+		
+		ArrayList<Integer> inter = new ArrayList<Integer>();
+		i1=0;i2=0;
+		while(i1<n1 && i2<n2) {
+			if(a1[i1]==a2[i2]) {
+				inter.add(a1[i1++]);
+				i2++;
+			} else if(a1[i1] < a2[i2]) {
+				i1++;
+			} else {
+				i2++;
+			}
+		}
+		
+		for(Integer i:inter)
+			System.out.print(i +" ");
+		
+		s.close();
+	}
+}
